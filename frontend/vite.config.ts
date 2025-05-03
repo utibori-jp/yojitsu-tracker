7b7fb7eabbd5f678bf16505a75c3828b87163912
HirokiMitani <sunvalley0802@gmail.com>
2025-05-03 08:33:55 +0000

    change framework

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server:{host:true}
+  plugins: [react()],
+  server: {
+    host: true,
+  },
 });
