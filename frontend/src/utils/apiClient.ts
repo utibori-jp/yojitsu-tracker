2ea8b9b3493c5fc9ea2f5a9d4e798a09174265fa
HirokiMitani <sunvalley0802@gmail.com>
2025-05-16 13:31:16 +0000

    add .env

--- frontend/src/utils/apiClient.ts
+++ frontend/src/utils/apiClient.ts
@@ -1,3 +1,3 @@
 import { createApiClient } from "../generated/api";
 
-export const apiClient = createApiClient("http://localhost:8080/api");
+export const apiClient = createApiClient(import.meta.env.VITE_API_BASE_URL);
