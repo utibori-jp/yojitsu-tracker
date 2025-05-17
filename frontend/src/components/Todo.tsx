a49a1a1196b02cba3f1560cbb42675e81e9398f9
HirokiMitani <sunvalley0802@gmail.com>
2025-05-17 05:33:21 +0000

    add: common component

--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -7,6 +7,7 @@ import {
   CheckCircleIcon,
   TrashIcon,
 } from "@heroicons/react/24/solid";
+import IconButton from "./IconButton";
 
 interface Props {
   todo: Todo;
@@ -42,44 +43,40 @@ const TodoItem: React.FC<Props> = ({ todo }) => {
         <div className="flex gap-2">
           <div>
             {status === "todo" || status === "pending" ? (
-              <button
+              <IconButton
                 title="開始"
                 onClick={handleStartStop}
-                className="border border-gray-300 bg-gray-100 hover:bg-gray-200 text-green-600 rounded-lg p-2"
-              >
-                <PlayIcon className="h-5 w-5" />
-              </button>
+                icon={PlayIcon}
+                color="text-green-600"
+              />
             ) : null}
             {status === "doing" ? (
-              <button
+              <IconButton
                 title="一時停止"
                 onClick={handleStartStop}
-                className="border border-gray-300 bg-gray-100 hover:bg-gray-200 text-yellow-600 rounded-lg p-2"
-              >
-                <PauseIcon className="h-5 w-5" />
-              </button>
+                icon={PauseIcon}
+                color="text-yellow-600"
+              />
             ) : null}
           </div>
-          <button
+          <IconButton
             title="完了"
             onClick={onCheck}
-            className="border border-gray-300 bg-gray-100 hover:bg-gray-200 text-purple-600 rounded-lg p-2"
-          >
-            <CheckCircleIcon className="h-5 w-5" />
-          </button>
-          <button
+            icon={CheckCircleIcon}
+            color="text-purple-600"
+          />
+          <IconButton
             title="削除"
             onClick={onDelete}
-            className="border border-gray-300 bg-gray-100 hover:bg-gray-200 text-red-600 rounded-lg p-2"
-          >
-            <TrashIcon className="h-5 w-5" />
-          </button>
-          <button
+            icon={TrashIcon}
+            color="text-red-600"
+          />
+          <IconButton
+            title="編集"
             onClick={onEdit}
-            className="border border-gray-300 bg-gray-100 hover:bg-gray-200 text-blue-600 rounded-lg p-2"
-          >
-            <PencilIcon className="h-5 w-5" />
-          </button>
+            icon={PencilIcon}
+            color="text-blue-600"
+          />
         </div>
       </div>
       <div className="flex space-x-4 mb-2">
